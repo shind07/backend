ec1142002b3303b26effb301b0b2f8b3d0328046
shind07 <shind@sfu.ca>
2018-11-15 10:10:53 -0800

    Modify histogram data source

--- tasks/histogram.py
+++ tasks/histogram.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
-from common import histogram_data as data, agateToArray
+from common import scatter_data as data, agateToArray
+
+NUM_SELECTS = 1
+columns = list(data.columns.keys());
+data_columns_start = 1
+defaults = columns[data_columns_start:data_columns_start+NUM_SELECTS]
 
 def func(**kwargs):
+    columns = defaults if 'cols' not in kwargs else kwargs['cols']
+    subset = data.select(columns)
     print("kwargs:", kwargs)
-    return agateToArray(data)
+    return agateToArray(subset)
 
 def select(**kwargs):
-    return list(data.columns.keys())[1:]
+    var = columns[data_columns_start:]
+    print(var)
+    return var
--- tasks/scatter.py
+++ tasks/scatter.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from common import scatter_data as data, agateToArray
 
-columns = data.columns.keys();
-defaults = columns[0:2]
+NUM_SELECTS = 1
+columns = list(data.columns.keys());
+data_columns_start = 1
+defaults = columns[data_columns_start:data_columns_start+NUM_SELECTS]
 
 def func(**kwargs):
     columns = defaults if 'cols' not in kwargs else kwargs['cols']
@@ -11,4 +13,6 @@ def func(**kwargs):
     return agateToArray(subset)
 
 def select(**kwargs):
-    return list(columns)
+    var = columns[data_columns_start:]
+    print(var)
+    return var
