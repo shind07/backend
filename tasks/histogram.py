0a1587f308cd45e5b183a5fabce5fd9d54eabf3a
shind07 <shind@sfu.ca>
2018-12-04 18:45:58 -0800

    Update histogram figure

--- tasks/histogram.py
+++ tasks/histogram.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-from common import nba_data as data, pandasToArray
+from common import relationships_data, pandasToArray
 import numpy as np
 
 NUM_SELECTS = 1
+data = relationships_data.sample(n=1300)
 columns = data.select_dtypes(np.number).columns.tolist()
 data_columns_start = 0
 defaults = columns[data_columns_start : data_columns_start + NUM_SELECTS]
+
 
 def func(**kwargs):
     columns = defaults if 'col' not in kwargs else kwargs['col']
--- tasks/relationships.py
+++ tasks/relationships.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
-from common import relationships_data as data, pandasToArray
+from common import relationships_data, pandasToArray
 
 NUM_SELECTS = 2
+data = relationships_data.sample(n=2000)
 columns = list(data.columns);
 data_columns_start = 0
 defaults = columns[data_columns_start:data_columns_start+NUM_SELECTS]
+
 
 def func(**kwargs):
     cols= defaults if 'cols' not in kwargs else kwargs['cols']
