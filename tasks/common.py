92f5dd8f2e0b4ce6260f44ceb8dd8b6b602a55f0
shind07 <shind@sfu.ca>
2018-12-07 12:05:41 -0800

    Remove unneccessary files

--- tasks/common.py
+++ tasks/common.py
@@ -1,4 +1,4 @@
-import csv, os, agate
+import csv, os
 import pandas as pd
 from decimal import Decimal
 from config import data_dir
@@ -8,27 +8,10 @@ def csvToArray(path):
         csv_reader = csv.reader(f)
         return [row for row in csv_reader]
 
-def agateToArray(table):
-    return [list(table.column_names)] + [
-        [round(float(i), 2) if isinstance(i, Decimal) else i if i is not None else "" for i in row]
-        for row in table]
-
 def pandasToArray(table):
     columns = list(table.columns) if type(table.columns) != pd.MultiIndex else \
         [table.columns[0][0]] + [col[1] for col in table.columns[1:]]
     return [columns] + table.round(2).values.tolist()
-
-
-
-tester = agate.TypeTester(force={
-    'Year': agate.Text()
-})
-
-sample_data = csvToArray(os.path.join(data_dir, 'sample.csv'))
-
-
-nba_data = pd.read_csv(os.path.join(data_dir, 'nba_trends.csv'))
-nba_data['Team'] = nba_data['Team'].apply(lambda team: team[:-1] if '*' in team else team)
 
 assists_action_data = pd.read_csv(os.path.join(data_dir, 'assists_action.csv'))
 assists_player_data = pd.read_csv(os.path.join(data_dir, 'assists_player.csv'))
